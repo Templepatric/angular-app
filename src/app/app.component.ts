693840b1f054a9a43343993fb1157c13740ee7a3
goodnesskanu <goodnesskanu550@gmail.com>
2023-06-14 08:54:06 -0700

    my -angular-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-angular-app';
 }
+console.log("Login");
+
+alert("Login");
+
+
+var FullName = document.getElementById('NAME');
+let PassWord = document.getElementById('password')
+var Email = document.getElementById('email')
+var displaybutton = document .getElementById('pass')
+
+
+var myFullName = ('your full name');{
+var myEmail = ('email');
+var myPassword = ('password');
+var errormsg = ('error');
+}
+
+
